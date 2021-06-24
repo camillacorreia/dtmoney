22c1785474c7b0453e963b59dadd1051b78b2125
Camilla Correia <camillajesuscorreia@hotmail.com>
2021-06-24 19:35:10 -0300

    feat: create component header

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
+import { Header } from './componentes/Header';
+import GlobalStyle from './styles/global';
+
 export function App() {
   return (
-    <h1>dtmoney</h1>
+    <>
+      <Header />
+      <GlobalStyle />
+    </>
   );
 }
